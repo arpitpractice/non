1548509817b14b04061e6dc09e806f97ccef1ab0
Arpit Anand <mjarpitanand@gmail.com>
2017-10-04 16:57:44 +0530

    Some backend done

---
+++ core/core.inc.php
@@ -0,0 +1,17 @@
+<?php
+    require_once 'connect.inc.php';
+?>
+
+<?php
+    session_start();
+    ob_start();
+    
+    
+    function sessionset(){
+        if(isset($_SESSION['id']) && !empty($_SESSION['id'])){
+            return true;
+        }else{
+            return false;
+        }
+    }
+?>
